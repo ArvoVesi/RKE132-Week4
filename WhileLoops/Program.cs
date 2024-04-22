8c21c5bd97a19e4f2703c757ff1180a3e0777af5
ArvoVesi <arvo.vesi@tktk.ee>
2024-04-22 11:24:14 +0300

    add week 4 code

---
+++ WhileLoops/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Random rnd = new Random();
+int cpuRandom;
+
+bool loopActive = true; // true / false
+
+while (loopActive)
+{
+    cpuRandom = rnd.Next(1, 4);
+    Console.WriteLine("Paku suvaline arv, siseta arv ühest kolmeni:");
+    
+    int userNumber = Int32.Parse(Console.ReadLine());
+
+    if (userNumber == cpuRandom)
+    {
+        Console.WriteLine("Palju õnne :DDDDDDDDDDDDD");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Paku uuesti :D");
+    }
+}
+
+Console.WriteLine("Head päeva !");
